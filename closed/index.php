b9ea5212751bb0dfab833e288252aa1c5a254b46
Marc Durdin <marc@durdin.net>
2019-12-18 09:15:03 +1100

    feat: forum transfer

---
+++ closed/index.php
@@ -0,0 +1,20 @@
+<html>
+  <head>
+    <title>Tavultesoft Keyman has moved</title>
+  </head>
+<body>
+
+<h1>Support and Downloads for Keyman</h1>
+
+<p>Keyman is now owned and distributed by <a href='https://sil.org'>SIL International</a>.</p>
+
+<p>
+  All content for Keyman is now available at <a href='https://keyman.com/'>keyman.com</a>. Keyman Desktop
+  and Keyman Developer are now free and open source, and downloads and free static activation keys for older
+  versions of Keyman are available also on the <a href='https://keyman.com/downloads/archive/'>Keyman downloads archive</a>.
+</p>
+
+<p>Technical support for Keyman is available through the <a href='https://community.software.sil.org/c/keyman'>Community Forum</a>.</p>
+
+</body>
+</html>
